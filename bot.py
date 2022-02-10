125d50eb69c5bb81311156c545cf722a6eceb322
CheeseFan40 <89537997+CheeseFan40@users.noreply.github.com>
2022-02-10 17:44:20 -0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import random
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+
+    async def on_message(self, message):
+        counter = 0
+        
+        list = ["Your number is:","Number:","Your next number is:"]
+        Random = random.choice(list)
+        
+        
+        
+        if message.author == self.user:
+            return
+        
+        if message.content == '$Count' or message.content == '$count':
+            try:
+                f = open("counter.txt", "r")
+                counter = int(f.readline())
+            except:
+                f = open("counter.txt", "a")
+                f.write(str(0))
+            counter+=1
+            f = open("counter.txt","w")
+            f.write(str(counter))
+                         
+            
+            await message.channel.send(Random + " " + str(counter))
+
+client = MyClient()
+client.run('')
